57d88df80367910d709554993ec46c6dc19614b1
Daemon Jung <jjjssswww1006@gmail.com>
2024-06-17 03:23:40 +0900

    webpack-dev-server

--- exercise/webpack.config.js
+++ exercise/webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  // mode는 vue3부터 선언할 필요없음
   mode: 'production',
   entry: './src/main.js',
   output: {
@@ -27,6 +28,7 @@ module.exports = {
           // other vue-loader options go here
         }
       },
+        // 최대한 최신 문법들을 적용할 수 있도록 함
       {
         test: /\.js$/,
         loader: 'babel-loader',
@@ -45,6 +47,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     },
+    // math.js 같은 외부라이브러리는 선언할 때 굳이 신경쓰지 않아도 됨
     extensions: ['*', '.js', '.vue', '.json']
   },
   devServer: {
@@ -58,23 +61,25 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports.devtool = '#source-map'
-//   // http://vue-loader.vuejs.org/en/workflow/production.html
-//   module.exports.plugins = (module.exports.plugins || []).concat([
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         NODE_ENV: '"production"'
-//       }
-//     }),
-//     new webpack.optimize.UglifyJsPlugin({
-//       sourceMap: true,
-//       compress: {
-//         warnings: false
-//       }
-//     }),
-//     new webpack.LoaderOptionsPlugin({
-//       minimize: true
-//     })
-//   ])
-// }
+// npm run build를 할 때 개발용으로 진행하겠음
+// webpack version3까지는 webpack 파슬 모델 번들러
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
